dcde37fb6b6e539dc733d3a851cd6da624764e3b
zhousy <33384217+PWFAAI@users.noreply.github.com>
2019-03-26 16:50:11 -0700

    Add files via upload
    both X and Y plane

--- transverseForceReadQpic.m
+++ transverseForceReadQpic.m
@@ -1,12 +1,25 @@
-function [ex,by] = transverseForceReadQpic(path,file,tstep)
-% read the transverse field, ex and by, in XZ plane
-    path_fex = [path,file,'FEX-XZ\FEX-XZ_',num2str(tstep,'%04d'),'.h5'];
-    name_fex = ['/FEX-XZ-',num2str(tstep,'%04d')];
-    hdf5info(path_fex);
-    ex = hdf5read(path_fex,name_fex);
+function [e,b] = transverseForceReadQpic(path,file,tstep,plane)
 
-    path_fby = [path,file,'FBY-XZ\FBY-XZ_',num2str(tstep,'%04d'),'.h5'];
-    name_fby = ['/FBY-XZ-',num2str(tstep,'%04d')];
-    hdf5info(path_fby);
-    by = hdf5read(path_fby,name_fby);
+% read the transverse field, ex and by, in XZ plane
+    if plane=='X'
+        path_e = [path,file,'FEX-XZ\FEX-XZ_',num2str(tstep,'%04d'),'.h5'];
+        name_e = ['/FEX-XZ-',num2str(tstep,'%04d')];
+        hdf5info(path_e);
+        e = hdf5read(path_e,name_e);
+
+        path_b = [path,file,'FBY-XZ\FBY-XZ_',num2str(tstep,'%04d'),'.h5'];
+        name_b = ['/FBY-XZ-',num2str(tstep,'%04d')];
+        hdf5info(path_b);
+        b = hdf5read(path_b,name_b);
+    else
+        path_e = [path,file,'FEY-YZ\FEY-YZ_',num2str(tstep,'%04d'),'.h5'];
+        name_e = ['/FEY-YZ-',num2str(tstep,'%04d')];
+        hdf5info(path_e);
+        e = hdf5read(path_e,name_e);
+
+        path_b = [path,file,'FBX-YZ\FBX-YZ_',num2str(tstep,'%04d'),'.h5'];
+        name_b = ['/FBX-YZ-',num2str(tstep,'%04d')];
+        hdf5info(path_b);
+        b = hdf5read(path_b,name_b);
+    end
 end
